7846c65ac26a6d3d9e8e335558a336de1b63a2ed
Jack McNicol <jackmcpickle@gmail.com>
2021-04-20 21:06:43 +0930

    initial postcss plugin setup

---
+++ index.js
@@ -0,0 +1,29 @@
+/* eslint-disable no-unused-vars */
+module.exports = (opts = { }) => {
+
+  // Work with options here
+
+  return {
+    postcssPlugin: 'postcss-include-media',
+    /*
+    Root (root, postcss) {
+      // Transform CSS AST here
+    }
+    */
+
+    /*
+    Declaration (decl, postcss) {
+      // The faster way to find Declaration node
+    }
+    */
+
+    /*
+    Declaration: {
+      color: (decl, postcss) {
+        // The fastest way find Declaration node if you know property name
+      }
+    }
+    */
+  }
+}
+module.exports.postcss = true
