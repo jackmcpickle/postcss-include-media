e2c62fb9c345b737a955d76cce3252ae5b927a5a
Jack McNicol <jackmcpickle@gmail.com>
2021-05-01 10:35:13 +0930

    update rollup config with ts

--- rollup.js
+++ rollup.js
@@ -1,4 +1,5 @@
 import babel from '@rollup/plugin-babel';
+import typescript from '@rollup/plugin-typescript';
 
 export default {
     input: 'index.js',
@@ -7,6 +8,7 @@ export default {
         { file: 'index.es.mjs', format: 'es', sourcemap: true, exports: 'default' },
     ],
     plugins: [
+        typescript(),
         babel({
             babelHelpers: 'bundled',
             plugins: ['@babel/plugin-syntax-dynamic-import'],
