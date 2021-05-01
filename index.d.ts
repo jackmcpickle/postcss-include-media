3a26ecf1d9605140283554802f8bb9f043dd253e
Jack McNicol <jackmcpickle@gmail.com>
2021-05-01 11:55:33 +0930

    update base types

--- index.d.ts
+++ index.d.ts
@@ -1,17 +1,27 @@
 // Type definitions for postcss-include-media 1.0
-// TypeScript Version: 2.2
+// TypeScript Version: 4.0
+
+export type BreakpointType = Record<string, string>;
+
+export type MediaExpressionType = Record<string, string>;
+
+export type UnitIntervalType = Record<string, number>;
+
+declare enum Operators {}
+
+export type OperatorsType = '>=' | '>' | '<=' | '<' | '≥' | '≤' | string;
+
+export interface IncludeMediaOptions {
+    breakpoints?: BreakpointType;
+    mediaExpressions?: MediaExpressionType;
+    unitIntervals?: UnitIntervalType;
+}
 
 import { PluginCreator } from 'postcss';
 
 declare namespace includemedia {
-    interface IncludeMediaOptions {
-        breakpoints?: Record<key, string>;
-        mediaExpressions?: Record<key, string>;
-        unitIntervals?: Record<key, number>;
-    }
-
     type IncludeMedia = PluginCreator<IncludeMediaOptions>;
 }
 
 declare const includemedia: includemedia.IncludeMedia;
-export = includemedia;
+export default includemedia;
