ca54c1661592463e92ad747dd0083187aa785440
Jack McNicol <jackmcpickle@gmail.com>
2021-05-01 11:54:55 +0930

    fix entry with ts

--- rollup.js
+++ rollup.js
@@ -2,7 +2,7 @@ import babel from '@rollup/plugin-babel';
 import typescript from '@rollup/plugin-typescript';
 
 export default {
-    input: 'index.js',
+    input: 'index.ts',
     output: [
         { file: 'index.cjs.js', format: 'cjs', sourcemap: true, exports: 'default' },
         { file: 'index.es.mjs', format: 'es', sourcemap: true, exports: 'default' },
